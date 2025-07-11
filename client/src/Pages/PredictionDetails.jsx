963022c5f3e22c35d563a09277c381687d92a14f
sanjairajaganapathi2005 <sanjairajaganapathi2005@gmail.com>
2025-07-11 08:32:29 +0530

    commit bug fixed

--- client/src/Pages/PredictionDetails.jsx
+++ client/src/Pages/PredictionDetails.jsx
@@ -1,5 +1,5 @@
 // --- PredictionDetails.jsx ---
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify';
 import { useLocation } from 'react-router-dom';
